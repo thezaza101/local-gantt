93bc2c447b9ff574083f48e1d59f5e11640201c2
Zahid <thezaza102@gmail.com>
2026-04-25 12:11:59 +1000

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /* Application Bootstrap */
 
-const APP_VERSION = "0.69";
-const APP_DATE = "2026-03-23";
+const APP_VERSION = "76";
+const APP_DATE = "2026-04-25";
 window.APP_VERSION = APP_VERSION;
 
 window.checkFileVersionWarning = function(data) {
